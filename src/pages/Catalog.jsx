3e2d3f6d7350ca222097ead7f2f8791a80d350a9
Huy <xinhtupeanh@gmail.com>
2022-09-22 17:45:32 +0700

    Re configuration

---
+++ src/pages/Catalog.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Helmet from '../components/Helmet';
+
+const Catalog = () => {
+	return <Helmet title="Sản phẩm">Catalog</Helmet>;
+};
+
+export default Catalog;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Helmet from '../components/Helmet';
+
+const Home = () => {
+	return <Helmet title="Trang chủ"></Helmet>;
+};
+
+export default Home;
