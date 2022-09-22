58bdd87a9d724058aa4b54f044cd8dbb9f76b981
Huy <xinhtupeanh@gmail.com>
2022-09-22 18:33:28 +0700

    1 React(Home/HeroSlider) | Design and Getdata for form Slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 import Helmet from '../components/Helmet';
 
+import HeroSlider from '../components/HeroSlider';
+
+import heroSliderData from '../assets/fake-data/hero-slider';
+
 const Home = () => {
-	return <Helmet title="Trang chủ"></Helmet>;
+	return (
+		<Helmet title="Trang chủ">
+			{/* Hero SLider */}
+			<HeroSlider data={heroSliderData} />
+			{/* End Hero SLider */}
+		</Helmet>
+	);
 };
 
 export default Home;
